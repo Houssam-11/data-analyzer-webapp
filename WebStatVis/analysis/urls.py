d39d3b8afab426427d31379883783b75b0d4d463
Houssam-11 <helberrouhi@gmail.com>
2025-01-16 02:39:39 +0100

    Delete WebStatVis directory

--- WebStatVis/analysis/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/', views.upload_file, name='upload_file'),
-    path('visualize/', views.visualize, name='visualize'),    
-]
-# from . means that the views module is imported from the current package (analysis).
